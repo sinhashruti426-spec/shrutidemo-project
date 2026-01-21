095babe7f4e06f7f4ccedd7978d03a0bec7a1e40
Shruti Sinha <sinhashruti426@gmail.com>
2026-01-22 02:06:30 +0530

    Create fundamentals.py

---
+++ basics/fundamentals.py
@@ -0,0 +1,26 @@
+# using variable
+a=12
+b=13
+sum=a+b
+print(sum)
+mode=a%b
+print(mode)
+div=a/b
+print(div)
+pow=a**b
+print(pow)
+
+# type casting
+a="2"
+b=3
+c=int(a)
+print(c+b)
+print(type(c))
+
+# user input
+a=input("Enter your Name: ")
+b=int(input("Enter your Age: "))
+c=float(input("Enter percentage: "))
+print("Name:",a)
+print("Age:",b)
+print("percentage:",c)
